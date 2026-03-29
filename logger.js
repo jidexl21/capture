4f8825ac55a3ca300e6fc6a301c645fea5488145
jidexl21 <32308886+jidexl21@users.noreply.github.com>
2026-03-29 15:28:12 +0100

    Added read from env for webhook

--- logger.js
+++ logger.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const log = require("./config.json");
-
+var logFile = process.env.WEBHOOK_LOG ?? log.logFile; 
 const Log = (data) =>{
    // console.log(data)
-    fs.appendFileSync(log.logfile, data);
+    fs.appendFileSync(logFile, data);
 }
 const View = ()=>{
     return fs.readFileSync(log.logfile, {encoding: "utf-8"});
